7005c188c9b6812a66f88e93763cc73502fc65f1
bkilimni <benjamin_kilimnik@brown.edu>
2021-07-20 20:47:30 -0400

    update build configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,15 @@
 module.exports = {
-  purge: {
-    content: [
-      "src/**/*.js",
-      "src/**/*.jsx",
-      "src/**/*.ts",
-      "src/**/*.tsx",
-      "public/**/*.html",
-    ],
-    options: {
-      safelist: ["/data-theme$/"],
-    },
-  },
+  purge: [
+    "src/**/*.js",
+    "src/**/*.jsx",
+    "src/**/*.ts",
+    "src/**/*.tsx",
+    "public/**/*.html",
+  ],
   theme: {
     extend: {
       colors: {
+        // primary: "#FF6363",
         primary: "#F50000",
         secondary: {
           100: "#E2E2D5",
@@ -24,6 +20,14 @@ module.exports = {
         body: ["Nunito"],
       },
     },
+  },
+  variants: {
+    textColor: ["responsive", "hover", "focus", "group-hover"],
+    backgroundColor: ["responsive", "hover", "focus", "active"],
+    background: ["responsive", "hover", "focus", "active"],
+    textColor: ["responsive", "hover", "focus", "active"],
+    visibility: ["responsive", "hover", "focus", "active", "group-hover"],
+    display: ["responsive", "hover", "focus", "active", "group-hover"],
   },
   plugins: [
     require("daisyui"),
