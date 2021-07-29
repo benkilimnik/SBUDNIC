a6e9367ef153584048283bf2a6aeadec99ed51ad
bkilimni <benjamin_kilimnik@brown.edu>
2021-07-28 20:36:26 -0400

    update team page

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -34,8 +34,33 @@ const ImageCard = ({ item }) => {
           ))}
         </div>
       </div> */}
+        <div class="h-full flex flex-col items-center text-center">
+          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-72 object-cover object-center mb-4" src={item.image}/>
+          <div class="w-full">
+            <h2 class="title-font font-medium text-lg text-gray-900">{item.title}</h2>
+            <h3 class="text-gray-500 mb-3">{item.category}</h3>
+            <p class="mb-4">{item.subtitle}</p>
+            <span class="inline-flex">
+              <a class="text-gray-500">
+                <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                </svg>
+              </a>
+              <a class="ml-2 text-gray-500">
+                <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                </svg>
+              </a>
+              <a class="ml-2 text-gray-500">
+                <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                </svg>
+              </a>
+            </span>
+          </div>
+        </div>
 
-        <div className="p-4">
+        {/* <div className="p-4">
           <div className="flex flex-col items-center justify-center h-full text-center sm:flex-row sm:justify-start sm:text-left">
             <img
               alt={item.title}
@@ -48,9 +73,6 @@ const ImageCard = ({ item }) => {
               </h2>
               <h3 className="mb-3 text-gray-500">
                 {item.category.split(",").map((tag) => (
-                  /* <span className="inline-block px-3 py-1 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">
-                  {tag}
-                </span> */
                   <span>{tag}</span>
                 ))}
               </h3>
@@ -98,7 +120,7 @@ const ImageCard = ({ item }) => {
               </span>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
   )
 }
--- src/components/ItemPages/Team.js
+++ src/components/ItemPages/Team.js
@@ -25,7 +25,7 @@ export default function Team() {
               haven't heard of them.
             </p>
           </div>
-              <div className="grid grid-cols-1 gap-2 xl:grid-cols-2 3xl:grid-cols-3 justify-items-left">
+              <div className="grid grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 justify-items-left">
                 {team.map((item) => (
                   <ImageCard key={item.id} item={item}></ImageCard>
                 ))}
