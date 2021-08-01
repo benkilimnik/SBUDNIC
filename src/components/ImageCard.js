6c692d4cc9f167ad685b486d52456dfc4c50e31f
bkilimni <benjamin_kilimnik@brown.edu>
2021-08-01 15:49:43 -0400

    speed up fadein

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -5,18 +5,18 @@ const ImageCard = ({ item }) => {
   const [showModal, setShowModal] = React.useState(false)
   return (
     <div key={item.id}>
-      <div class="h-full flex flex-col items-center text-center">
+      <div className="flex flex-col items-center h-full text-center">
         <img
           alt="team"
-          class="flex-shrink-0 rounded-lg w-10/12 h-72 object-cover object-center mb-4"
+          className="flex-shrink-0 object-cover object-center w-10/12 mb-4 rounded-lg h-72"
           src={item.image}
         />
-        <div class="w-full">
-          <h2 class="title-font font-medium text-lg text-gray-900">
+        <div className="w-full">
+          <h2 className="text-lg font-medium text-gray-900 title-font">
             {item.title}
           </h2>
-          <h3 class="text-gray-500 mb-3">{item.category}</h3>
-          <p class="mb-4 h-24 line-clamp">{item.subtitle}</p>
+          <h3 className="mb-3 text-gray-500">{item.category}</h3>
+          <p className="h-24 mb-4 line-clamp">{item.subtitle}</p>
           <div className="flex flex-col items-center">
             <button
               // className="px-3 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase transition-all duration-75 ease-in bg-indigo-500 rounded shadow outline-none active:bg-indigo-600 hover:shadow-lg focus:outline-none"
@@ -52,7 +52,7 @@ const ImageCard = ({ item }) => {
                     <div className="flex flex-col items-center">
                       <img
                         alt="team"
-                        class="hidden sm:flex-shrink-0 sm:rounded-lg sm:w-5/12 sm:my-12 sm:h-80 sm:object-cover sm:object-center sm:mb-4"
+                        className="hidden sm:flex-shrink-0 sm:rounded-lg sm:w-5/12 sm:my-12 sm:h-80 sm:object-cover sm:object-center sm:mb-4"
                         src={item.image}
                       />
                     </div>
@@ -71,40 +71,40 @@ const ImageCard = ({ item }) => {
                         Close
                       </button>
                       <div className="grid grid-cols-3 pb-2">
-                        <button class="mx-1 text-gray-500">
+                        <button className="mx-1 text-gray-500">
                           <svg
                             fill="none"
                             stroke="currentColor"
                             stroke-linecap="round"
                             stroke-linejoin="round"
                             stroke-width="2"
-                            class="w-5 h-5"
+                            className="w-5 h-5"
                             viewBox="0 0 24 24"
                           >
                             <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                           </svg>
                         </button>
-                        <button class="mx-1 text-gray-500">
+                        <button className="mx-1 text-gray-500">
                           <svg
                             fill="none"
                             stroke="currentColor"
                             stroke-linecap="round"
                             stroke-linejoin="round"
                             stroke-width="2"
-                            class="w-5 h-5"
+                            className="w-5 h-5"
                             viewBox="0 0 24 24"
                           >
                             <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                           </svg>
                         </button>
-                        <button class="mx-1 text-gray-500">
+                        <button className="mx-1 text-gray-500">
                           <svg
                             fill="none"
                             stroke="currentColor"
                             stroke-linecap="round"
                             stroke-linejoin="round"
                             stroke-width="2"
-                            class="w-5 h-5"
+                            className="w-5 h-5"
                             viewBox="0 0 24 24"
                           >
                             <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
@@ -117,38 +117,38 @@ const ImageCard = ({ item }) => {
               </div>
             </>
           ) : null}
-          <span class="inline-flex">
-            <a href="/" class="text-gray-500">
+          <span className="inline-flex">
+            <a href="/" className="text-gray-500">
               <svg
                 fill="none"
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                class="w-5 h-5"
+                className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
               </svg>
             </a>
-            <a href="/" class="ml-2 text-gray-500">
+            <a href="/" className="ml-2 text-gray-500">
               <svg
                 fill="none"
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                class="w-5 h-5"
+                className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
                 <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
               </svg>
             </a>
-            <a href="/" class="ml-2 text-gray-500">
+            <a href="/" className="ml-2 text-gray-500">
               <svg
                 fill="none"
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                class="w-5 h-5"
+                className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
                 <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
