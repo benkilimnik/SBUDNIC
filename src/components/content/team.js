c68ac9c85277d29aa41c4d3943453cb86e70e16e
bkilimni <benjamin_kilimnik@brown.edu>
2021-08-01 16:14:18 -0400

    update team

--- src/components/content/team.js
+++ src/components/content/team.js
@@ -23,6 +23,13 @@ const team = [
     image: dheraj,
   },
   {
+    id: 8,
+    title: "Haley Rose Flores",
+    subtitle: "Haley Rose Flores is a first-year undergraduate student at Brown University studying Computer Science and Engineering. This is Haley’s first involvement in an engineering project at Brown University. During the summer before her senior year, Haley conducted research at the New Jersey Institute of Technology studying the process of self-assembly of multilayer graphene in water-containing impurities under the supervision of Dr. Dibakar Datta and Vidushi Sharma. As a part of the STEAM Academy throughout high school, Haley led the Green Technology House Project, which consisted of wiring a miniature house with solar panels, rechargeable batteries, and an Arduino. On the SBUDNIC team, Haley is a member of the Electrical Power System (EPS) subsystem and is responsible for designing and constructing the battery pack for the satellite alongside Brown alumnus, Shin-Ji Low.",
+    category: "Member of the EPS subsystem",
+    image: haley,
+  },
+  {
     id: 5,
     title: "Benjamin Kilimnik",
     subtitle: "A junior at Brown University studying computer science with a focus on systems research, data science, and web design. When I'm not coding up a hot mess in the Computer Science department, you can find me working at the Center for Computation and Visualization, researching for Systems@Brown or reading historical fiction next to a radiator.",
@@ -50,13 +57,6 @@ const team = [
     subtitle: "Rising junior from Framingham, MA studying Applied Math and Computer Science. As part of the OBDH team, I am working to ensure that our computer systems survive the radiation of space. Outside of school work I like to play hockey, build things, and reinvent the wheel.",
     category: "Onboard Data Handling Engineer",
     image: filip,
-  },
-  {
-    id: 8,
-    title: "Haley Rose Flores",
-    subtitle: "Haley Rose Flores is a first-year undergraduate student at Brown University studying Computer Science and Engineering. This is Haley’s first involvement in an engineering project at Brown University. During the summer before her senior year, Haley conducted research at the New Jersey Institute of Technology studying the process of self-assembly of multilayer graphene in water-containing impurities under the supervision of Dr. Dibakar Datta and Vidushi Sharma. As a part of the STEAM Academy throughout high school, Haley led the Green Technology House Project, which consisted of wiring a miniature house with solar panels, rechargeable batteries, and an Arduino. On the SBUDNIC team, Haley is a member of the Electrical Power System (EPS) subsystem and is responsible for designing and constructing the battery pack for the satellite alongside Brown alumnus, Shin-Ji Low.",
-    category: "Member of the EPS subsystem",
-    image: haley,
   },
   {
     id: 8,
