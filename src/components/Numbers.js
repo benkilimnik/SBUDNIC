b7a04ec60c7b43f4474e327206e7e596e5bcf3b0
bkilimni <benjamin_kilimnik@brown.edu>
2021-08-25 09:54:59 -0400

    add Ham Radio notice

--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -5,80 +5,46 @@ const Numbers = () => {
   return (
     <section className="text-gray-600 body-font">
       <div className="flex flex-col items-center px-8 mx-auto sm:py-12 lg:py-24">
-        {/* <div className="flex flex-wrap content-start mt-auto mb-auto -mx-4 lg:w-1/2 sm:w-2/3 sm:pr-10">
+        <div className="flex flex-wrap content-start mt-auto mb-auto -mx-4 lg:w-1/2 sm:w-2/3 sm:pr-10">
           <div className="w-full px-4 mb-6 sm:p-4">
-            <h1 className="mb-2 text-xl font-medium text-gray-900 title-font">
+            {/* <h1 class="md:text-3xl text-2xl font-medium title-font text-gray-900">
+                Looking for Ham Radio Operator
+              </h1>
+              <div className="leading-relaxed">
+                If you are a ham radio operator and are interested in sending
+                uplink or receiving downlink data, please send us an email!
+              </div> */}
+            <div
+              class="shadow-sm border-t-4 border-secondary rounded-b px-4 py-3"
+              role="alert"
+            >
+              <div class="flex">
+                <div class="py-1">
+                  <svg
+                    class="fill-primary h-6 w-6 text-teal-500 mr-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+                  </svg>
+                </div>
+                <div>
+                  <h1 class="md:text-3xl text-2xl font-medium title-font text-gray-900">
+                    Looking for Ham Radio Operator
+                  </h1>
+                  <div className="py-2 text-sm leading-relaxed md:text-lg">
+                    If you are a ham radio operator and are interested in
+                    sending uplink or receiving downlink data, please send us an
+                    email at <a href="mailto:SBUDNIC@listserv.brown.edu" className="text-blue-500">SBUDNIC@listserv.brown.edu</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* <h1 className="mb-2 text-xl font-medium text-gray-900 title-font">
               Stay in the loop
-            </h1>
-
-            <div className="leading-relaxed">
-              Follow us on Twitter, Instagram, and LinkedIn.
-            </div>
-       
-            <div className="flex flex-row items-center py-4 mt-4 align-center">
-              <a
-                href="https://twitter.com/SbudnicS"
-                className="transition duration-500 ease-out transform hover:text-indigo-600 hover:scale-110 hover:opacity-100"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <svg
-                  fill="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  className="w-7 h-7"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-              <a
-                href="https://instagram.com/sbudnics"
-                className="ml-5 transition duration-500 ease-out transform hover:text-indigo-600 hover:scale-110 hover:opacity-100"
-                rel="noreferrer"
-                target="_blank"
-              >
-                {" "}
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  className="w-7 h-7"
-                  viewBox="0 0 24 24"
-                >
-                  <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                  <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                </svg>
-              </a>
-              <a
-                href="https://www.linkedin.com/company/sbudnic/"
-                className="ml-5 transition duration-500 ease-out transform hover:text-indigo-600 hover:scale-110 hover:opacity-100"
-                rel="noreferrer"
-                target="_blank"
-              >
-                {" "}
-                <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="0"
-                  className="w-7 h-7"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="none"
-                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-                  ></path>
-                  <circle cx="4" cy="4" r="2" stroke="none"></circle>
-                </svg>
-              </a>
-            </div>
+            </h1> */}
           </div>
-        </div> */}
+        </div>
 
         <div className="w-full mt-6 overflow-hidden rounded-lg lg:w-6/12 sm:w-1/3 sm:mt-0">
           <div className="flex flex-col items center">
@@ -88,14 +54,13 @@ const Numbers = () => {
               options={{ height: 600 }}
             />
             <div className="py-4">
-                <TwitterFollowButton
-                  screenName="SbudnicS"
-                  options={{ size: "large" }}
-                />
+              <TwitterFollowButton
+                screenName="SbudnicS"
+                options={{ size: "large" }}
+              />
             </div>
           </div>
         </div>
-
       </div>
     </section>
   )
